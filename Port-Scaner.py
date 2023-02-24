62e95a2820e562cfae54dad7174acc0d92e965f8
Andreu Seguí <andreuseguisegura@gmail.com>
2023-02-24 19:12:01 +0100

    Major changes and fixed bugs

--- Port-Scaner.py
+++ Port-Scaner.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 
 import socket
+import subprocess
 from concurrent.futures import ThreadPoolExecutor, as_completed
 import datetime
 import argparse
 import time
 import os
+
 
 # Constantes
 BANNER = """
@@ -19,18 +21,18 @@ By: Andreu Seguí Segura
 """
 TIMEOUT = 30
 
-# Lista donde se almacenan los puertos abiertos
-ports = []
+# Diccionario vacio para almacenar las ips y los puertos
+hosts = {}
 
 def flags():
     # Creamos el parseador
     parser = argparse.ArgumentParser()
     # Añadimos los argumentos
-    parser.add_argument("-s", "--server", required=True, type=str, help="Ip del servidor o de la red")
-    parser.add_argument("-t", "--threads", required=True, type=int, help="Cantidad de hilos trabajando simultaneos. Cuanto mas alto sea el numero mas velocidad de escaneo")
-    parser.add_argument("-minp", "--minport", required=True, type=int, help="El primer puerto que vas a escanear. El numeor minimo es 1")
-    parser.add_argument("-maxp", "--maxport", required=True, type=int, help="El ultimo puerto que vas a escanear. El numero maximo es 65535")
-    parser.add_argument("-l", "--log", required=False, action="store_true", help="Si indicas este argumento se generara un log")
+    parser.add_argument("-s", "--server", required=True, type=str, help="Network IP or Target IP")
+    parser.add_argument("-t", "--threads", required=True, type=int, help="Threads working to scann. The more threads, the faster will be")
+    parser.add_argument("-minp", "--minport", required=True, type=int, help="First port to scann. Minimum 1")
+    parser.add_argument("-maxp", "--maxport", required=True, type=int, help="Last port to scann. Maximum 65535")
+    parser.add_argument("-l", "--log", required=False, action="store_true", help="Generating a log")
     # Parseamos los arumentos al script
     args = parser.parse_args()
     # Devlovemos la variable del nombre del archivo
@@ -43,96 +45,101 @@ def scan_host(ip, port):
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         # Establece un tiempo de espera para la conexión
         sock.settimeout(TIMEOUT)
-        result = sock.connect_ex((ip, port))
-        host = socket.gethostbyaddr(ip)
-        # Si te escaneas a ti mismo, que salga la ip 127.0.0.1
-        if host[2][0] == '::1':
-            host[2][0] = '127.0.0.1'
-        # Muestra el puerto si la conexión es exitosa
-        if result == 0:
-            try:
-                serv = socket.getservbyport(port)
-            except Exception:
-                serv = 'Nombre no encontrado'
-            print('[\033[32m+\033[0m] Ip: {} | Hostname: {} | Puerto: {} [Open] [{}]'.format(host[2][0],host[0], port, serv))
-            ports.append(port)
-        # Cierra el socket
-        sock.close()
-    except Exception as error:
-        print('[\033[31m-\033[0m]Error: {} | Ip: {}'.format(error, ip))
+        output = subprocess.check_output(["ping", "-c", "1", ip])
+        if "1 received" in output.decode():
+            result = sock.connect_ex((ip, port))
+            # Muestra el puerto si la conexión es exitosa
+            if result == 0:
+                try:
+                    serv = socket.getservbyport(port)
+                except Exception:
+                    serv = 'Service not found'
+                print('[\033[32m+\033[0m] IP: {} | Port: [{}/{}] OPEN'.format(ip, port, serv))
+                # Si no existe la ip en el diccionario agrega un lista de puertos vacia
+                if ip not in hosts:
+                    hosts[ip] = []
+                # Agrega el puerto a la lista de puertos para esta IP
+                hosts[ip].append(port)
+            # Cierra el socket
+            sock.close()
+        else:
+            pass
+    except Exception:
+        pass
 
 
 def generate_log():
     date = datetime.datetime.now()
     date_formated = date.strftime("%Y-%m-%d %H:%M:%S")
-    with open('ports-{}.txt'.format(date_formated), 'w+') as archivo:
-        archivo.write('Los puertos {} están abiertos en la dirección IP {}\nDia y Hora: {}'.format(ports, args.server, date_formated))
+    with open('Log-{}.log'.format(date_formated), 'w+') as file:
+        file.write('Date: {}\n\n'.format(date_formated))
+        for key, value in hosts.items():
+            file.write('IP: {} | PORTS: {}\n'.format(key, value))
+
 
 if __name__ == '__main__':
     os.system('clear')
     print(BANNER)
     # Pasmos los argumentos
     args = flags()
-    print('Ip: {}'.format(args.server))
-    print('Hilos tabajando simultaneamente: {}'.format(args.threads))
-    print('Rango de puertos a escanear: {} - {}'.format(args.minport, args.maxport))
-    print('Timeout de: {} segundos\n'.format(TIMEOUT))
     # Establecemos un tiempo
     time_start = time.time()
+
     # Si los argumentos son invalidos, los definimos de manera que sean validos
     if args.maxport >= 65535:
         args.maxport = 65535
     if args.minport <= 1:
         args.minport = 1
+    
+    # Comprovamos que la ip proporcionada es una 0. Eso significa que es una red y no un host
     if args.server[-1] == '0':
+        # Creamos una lista de puertos mas usados, para no tener que comprobar todoslos puertos existentes
+        ports_to_scann = [20, 21, 22, 25, 53, 80, 110, 143, 443, 1433, 1434, 3306, 3307, 5432, 5433, 5900, 8000, 8080]
+        print('Network: {}'.format(args.server))
+        print('Threads Working: {}'.format(args.threads))
+        print('Ports To Scan: {}'.format(ports_to_scann))
+        print('Timeout: {}\n'.format(TIMEOUT))
+        # Recorremos por todas las Ips de la red
         for host in range(1, 254):
+            # Creamos una variable para almacenar el prefijo de la red
             network_prefix = args.server[:-1]
+            # Añadimos a cada host la ip correspondiendo segun el prefijo de la red
             host = network_prefix + str(host)
             with ThreadPoolExecutor(max_workers=args.threads) as executor:
-                futures = [executor.submit(scan_host,host, port) for port in range(args.minport, args.maxport)]
+                futures = [executor.submit(scan_host,host, port) for port in ports_to_scann]
                 for future in as_completed(futures):
                     pass
-        # Si no se encunetra ningun puerto, envia un mensaje de que no se ha encontrado nada
-        if len(ports) == 0:
-            print('No se han encontrado puertos')
-        # Si se ha encontrado hacer un informe
+        # Acabamos el tiempo para hacer un recuento total de los segundos que ha tardado el escaner
+        time_end = time.time()
+        total_time = time_end - time_start
+        print('\nThe network has been scanned in {} seconds\n'.format(total_time))
+        # Si se ha pasado el argumento de log se generara, si no no se generara nada
+        if args.log:
+            generate_log()
+            input('The log has been genereated. Press [enter] to close the program')
+            pass
         else:
-            # Acabamos el tiempo para hacer un recuento total de los segundos que ha tardado el escaner
-            time_end = time.time()
-            total_time = time_end - time_start
-            print('\nNumeros de puertos totales: {}'.format(len(ports)))
-            print('Estos puertos son: {}'.format(ports))
-            print('El objetivo ha sido escaneado en un total de {} segundos\n'.format(total_time))
-            # Si se ha pasado el argumento de log se generara, si no no se generara nada
-            if args.log:
-                generate_log()
-                input('Se ha generado un log. Pulsar intro para cerrar el programa')
-                pass
-            else:
-                input('No se ha generado un log. Pulsar intro para cerrar el programa')
-                pass
+            input('The log has not been generated. Press [enter] to close the program')
+            pass
     else:
+        print('Target IP: {}'.format(args.server))
+        print('Threads Working: {}'.format(args.threads))
+        print('Range of Ports To Scan {} - {}'.format(args.minport, args.maxport))
+        print('Timeout: {}\n'.format(TIMEOUT))
         # Definimos que trabajen todos hilos
         with ThreadPoolExecutor(max_workers=args.threads) as executor:
             futures = [executor.submit(scan_host,args.server, port) for port in range(args.minport, args.maxport)]
             for future in as_completed(futures):
                 pass
-        # Si no se encunetra ningun puerto, envia un mensaje de que no se ha encontrado nada
-        if len(ports) == 0:
-            print('No se han encontrado puertos')
-        # Si se ha encontrado hacer un informe
+        # Acabamos el tiempo para hacer un recuento total de los segundos que ha tardado el escaner
+        time_end = time.time()
+        total_time = time_end - time_start
+        print('The network has been scanned in {} seconds\n'.format(total_time))
+        # Si se ha pasado el argumento de log se generara, si no no se generara nada
+        if args.log:
+            generate_log()
+            input('The log has been genereated. Press [enter] to close the program')
+            pass
         else:
-            # Acabamos el tiempo para hacer un recuento total de los segundos que ha tardado el escaner
-            time_end = time.time()
-            total_time = time_end - time_start
-            print('\nNumeros de puertos totales: {}'.format(len(ports)))
-            print('Estos puertos son: {}'.format(ports))
-            print('El objetivo ha sido escaneado en un total de {} segundos\n'.format(total_time))
-            # Si se ha pasado el argumento de log se generara, si no no se generara nada
-            if args.log:
-                generate_log()
-                input('Se ha generado un log. Pulsar intro para cerrar el programa')
-                pass
-            else:
-                input('No se ha generado un log. Pulsar intro para cerrar el programa')
-                pass
+            input('The log has not been generated. Press [enter] to close the program')
+            pass
